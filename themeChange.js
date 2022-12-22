563ff8a2454d8207efcbceb3ace291e82ee05fff
antonio-ramon <antonio-ramon-dev@outlook.com>
2022-12-22 15:45:22 -0300

    switch-theme button saveState on LocalStorage

--- themeChange.js
+++ themeChange.js
@@ -1,13 +1,16 @@
-/** code by webdevtrick ( https://webdevtrick.com ) **/
 var checkbox = document.querySelector('input[name=mode]');
 
 checkbox.addEventListener('change', function () {
     if (this.checked) {
         trans()
         document.documentElement.setAttribute('data-theme', 'darktheme')
+        saveCheckboxState()
+        saveThemeState()
     } else {
         trans()
         document.documentElement.setAttribute('data-theme', 'lighttheme')
+        saveCheckboxState()
+        saveThemeState()
     }
 })
 
@@ -16,4 +19,26 @@ let trans = () => {
     window.setTimeout(() => {
         document.documentElement.classList.remove('transition');
     }, 1000)
+}
+
+// Função para salvar o estado caso o botão tenha sido pressionado ou não.
+function saveCheckboxState() {
+    var checkbox = document.getElementById('switch');
+    localStorage.setItem('checkboxState', checkbox.checked);
+}
+
+// Função para salvar o thema selecionao pelo usuário.
+function saveThemeState() {
+    var theme = document.documentElement.getAttribute('data-theme')
+    localStorage.setItem('themeState', theme)
+}
+
+// Carregar os dados salvos no localStorage do navegador.
+window.onload = function() {
+    var checkbox = document.getElementById('switch');
+    var checkboxState = localStorage.getItem('checkboxState');
+    checkbox.checked = checkboxState === 'true';
+
+    var themeState = localStorage.getItem('themeState');
+    document.documentElement.setAttribute('data-theme', themeState);
 }
