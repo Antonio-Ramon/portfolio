b2dff78c152af940e02515ba8b1513a19c818fc1
Antonio-Ramon <antonio-ramon-dev@outlook.com>
2023-01-26 09:19:47 -0300

    Refactor the themeChange function

--- themeChange.js
+++ themeChange.js
@@ -1,44 +1,30 @@
-var checkbox = document.querySelector('input[name=mode]');
-
-checkbox.addEventListener('change', function () {
-    if (this.checked) {
-        trans()
-        document.documentElement.setAttribute('data-theme', 'darktheme')
-        saveCheckboxState()
-        saveThemeState()
-    } else {
-        trans()
-        document.documentElement.setAttribute('data-theme', 'lighttheme')
-        saveCheckboxState()
-        saveThemeState()
-    }
-})
-
-let trans = () => {
-    document.documentElement.classList.add('transition');
-    window.setTimeout(() => {
-        document.documentElement.classList.remove('transition');
-    }, 1000)
+// Função para mudar o tema
+function toggleMode() {
+  // documentElement refere-se a tag <html> no index.html
+  const html = document.documentElement;
+  html.classList.toggle("dark");
+  saveCheckboxState();
+  saveThemeState();
 }
 
 // Função para salvar o estado caso o botão tenha sido pressionado ou não.
 function saveCheckboxState() {
-    var checkbox = document.getElementById('switch');
-    localStorage.setItem('checkboxState', checkbox.checked);
+  var checkbox = document.getElementById("switch");
+  localStorage.setItem("checkboxState", checkbox.checked);
 }
 
 // Função para salvar o thema selecionao pelo usuário.
 function saveThemeState() {
-    var theme = document.documentElement.getAttribute('data-theme')
-    localStorage.setItem('themeState', theme)
+  var theme = document.documentElement.getAttribute("class");
+  localStorage.setItem("class", theme);
 }
 
 // Carregar os dados salvos no localStorage do navegador.
-window.onload = function() {
-    var checkbox = document.getElementById('switch');
-    var checkboxState = localStorage.getItem('checkboxState');
-    checkbox.checked = checkboxState === 'true';
+window.onload = () => {
+  var checkbox = document.getElementById("switch");
+  var checkboxState = localStorage.getItem("checkboxState");
+  checkbox.checked = checkboxState === "true";
 
-    var themeState = localStorage.getItem('themeState');
-    document.documentElement.setAttribute('data-theme', themeState);
-}
+  var themeState = localStorage.getItem("class");
+  document.documentElement.setAttribute("class", themeState);
+};
